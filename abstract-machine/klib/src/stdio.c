070f1f9879a85ee30c0ab2ab086b62ef51938174
Rong "Mantle" Bao <baorong2005@126.com>
2024-03-07 23:41:33 +0800

    pa2: implement <string.h>, add simplified `sprintf` and `vsprintf`

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,40 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int len = 0;
+  const char *fmt_cur = fmt;
+  char *out_cur = out;
+  while (*fmt_cur != '\0') {
+    if (!strncmp(fmt_cur, "%s", 2)) {
+      char *str = va_arg(ap, char *);
+      int l = strlen(str);
+      strcat(out_cur, str);
+      out_cur += l;
+      fmt_cur += 2;
+      len += l;
+    } else if (!strncmp(fmt_cur, "%d", 2)) {
+      int val = va_arg(ap, int);
+      int l;
+      itoa(val, out_cur, 10, &l);
+      out_cur += l;
+      fmt_cur += 2;
+      len += l;
+    } else {
+      *(out_cur++) = *(fmt_cur++);
+      len++;
+    }
+  }
+  *out_cur = '\0';
+  len++;
+  return len;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+  int result = vsprintf(out, fmt, args);
+  va_end(args);
+  return result;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,6 +29,44 @@ int atoi(const char* nptr) {
   return x;
 }
 
+static void reverse(char *str, int length) {
+  int start = 0;
+  int end = length - 1;
+  while (start < end) {
+    str[start] ^= str[end];
+    str[end] ^= str[start];
+    str[start] ^= str[end];
+    end--;
+    start++;
+  }
+}
+
+char *itoa(int num, char *str, int base, int *len) {
+  int i = 0;
+  bool is_neg = false;
+  if (num == 0) {
+    str[i++] = '0';
+    str[i] = '\0';
+    return str;
+  }
+  if (num < 0 && base == 10) {
+    is_neg = true;
+    num = -num;
+  }
+  while (num != 0) {
+    int rem = num % base;
+    str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+    num /= base;
+  }
+  if (is_neg) {
+    str[i++] = '-';
+  }
+  str[i] = '\0';
+  reverse(str, i);
+  *len = i;
+  return str;
+}
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
