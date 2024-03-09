a5c0f6c75e2b4924c8cd72994af81b450707cf72
Rong "Mantle" Bao <baorong2005@126.com>
2024-03-09 09:49:12 +0800

    pa2: add keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,7 +3,11 @@
 
 #define KEYDOWN_MASK 0x8000
 
+static volatile uint32_t *const KEYBOARD_PORT_BASE = (volatile uint32_t *)0xa0000060;
+
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t key = KEYBOARD_PORT_BASE[0];
+
+  kbd->keydown = key & KEYDOWN_MASK;
+  kbd->keycode = key & ~KEYDOWN_MASK;
 }
