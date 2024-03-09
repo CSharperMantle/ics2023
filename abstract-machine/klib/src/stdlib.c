a2060a1b328102c0fa07a3f5c53907ec6c82e2d2
Rong "Mantle" Bao <baorong2005@126.com>
2024-03-09 14:21:29 +0800

    pa2: implement simple malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -31,14 +31,16 @@ int atoi(const char *nptr) {
   return x;
 }
 
+static void *malloc_last_addr = NULL;
+
 void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+  if (malloc_last_addr == NULL) {
+    malloc_last_addr = heap.start;
+  }
+  size_t size_adj = size & 0xF ? (size & ~(size_t)0xF) + 0x10 : size;
+  void *ptr = malloc_last_addr;
+  malloc_last_addr = (uint8_t *)malloc_last_addr + size_adj;
+  return ptr;
 }
 
 void free(void *ptr) {}
