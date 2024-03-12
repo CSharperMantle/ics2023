643f14b7cfb9f8a984859c4546d9a986a6545fef
Rong "Mantle" Bao <baorong2005@126.com>
2024-03-12 16:38:34 +0800

    chore: improve ftrace message format

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -1,33 +1,38 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
-#include <isa.h>
 #include "local-include/reg.h"
+#include <isa.h>
 
-const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
+const char *regs[] = {"$0", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
+                      "a1", "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
+                      "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+
+#define FMT_INT_LADJ_W_ MUXDEF(CONFIG_ISA64, "%-*" PRId64, "%-*" PRId32)
 
 void isa_reg_display() {
   for (int i = 0; i < ARRLEN(regs); i++) {
-    printf("%s " FMT_WORD " " FMT_WORD_INT "\n", reg_name(i), gpr(i), gpr(i));
+    printf("%-*s " FMT_WORD " " FMT_INT_LADJ_W_ "%c",
+           16,
+           reg_name(i),
+           gpr(i),
+           16,
+           gpr(i),
+           i % 2 == 0 ? ' ' : '\n');
   }
-  printf("pc " FMT_WORD "\n", cpu.pc);
+  printf("%-*s " FMT_WORD "\n", 16, "pc", cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
