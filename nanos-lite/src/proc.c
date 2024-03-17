643af135216ba9ca4bbe68e13067a90c2ba2bfdd
Rong "Mantle" Bao <baorong2005@126.com>
2024-03-17 15:59:41 +0800

    pa3: add calls to init/fini array

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc(void) {
   Log("Initializing processes...");
 
   // TODO: load program here
-  naive_uload(NULL, "/bin/nplayer");
+  naive_uload(NULL, "/bin/cpp-test");
 }
 
 Context *schedule(Context *prev) {
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,12 +1,21 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <assert.h>
 
-int main(int argc, char *argv[], char *envp[]);
+extern void __libc_init_array(void);
+extern void __libc_fini_array(void);
+
+extern int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
+
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
+  char *empty[] = {NULL};
   environ = empty;
+
+  atexit(__libc_fini_array);
+
+  __libc_init_array();
   exit(main(0, empty, empty));
-  assert(0);
+
+  assert(("unreachable", 0));
 }
