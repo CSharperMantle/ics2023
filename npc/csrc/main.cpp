550f813c17b8ab38d27ca450e32e5dea8095b3aa
Rong "Mantle" Bao <baorong2005@126.com>
2024-04-11 22:36:29 +0800

    ysyx-pre: add seq lights to npc

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -2,25 +2,48 @@
 #undef NDEBUG
 #endif
 
-#include <iostream>
-#include <cstdlib>
-#include <cassert>
-#include <nvboard.h>
-#include "Vtop.h"
+#include "Vlight.h"
 #include "verilated.h"
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <nvboard.h>
 
-extern void nvboard_bind_all_pins(Vtop* top);
+typedef Vlight Dut;
+
+extern void nvboard_bind_all_pins(Dut *top);
+
+static void single_cycle(Dut &dut) {
+  dut.clk = 0;
+  dut.eval();
+  nvboard_update();
+  dut.clk = 1;
+  dut.eval();
+  nvboard_update();
+}
+
+static void reset(Dut &dut, int n) {
+  dut.rst = 1;
+  while (n-- > 0) {
+    single_cycle(dut);
+  }
+  dut.rst = 0;
+}
 
 int main(int argc, char *argv[]) {
-    VerilatedContext ctx{};
-    ctx.commandArgs(argc, argv);
-    Vtop top{&ctx};
-    nvboard_bind_all_pins(&top);
-    nvboard_init();
-    while (!ctx.gotFinish()) {
-        top.eval();
-        nvboard_update();
-    }
-    nvboard_quit();
-    return 0;
+  VerilatedContext ctx{};
+  ctx.commandArgs(argc, argv);
+
+  Dut dut{&ctx};
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  reset(dut, 10);
+  while (!ctx.gotFinish()) {
+    single_cycle(dut);
+  }
+
+  nvboard_quit();
+
+  return 0;
 }
