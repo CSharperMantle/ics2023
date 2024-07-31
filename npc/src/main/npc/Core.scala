a6cf83d459b02f5aab7a9faf70ca157c565346d2
Rong "Mantle" Bao <baorong2005@126.com>
2024-07-31 22:50:26 +0800

    ysyx: add simple xbar

--- npc/src/main/npc/Core.scala
+++ npc/src/main/npc/Core.scala
@@ -29,16 +29,36 @@ class Core extends Module {
   val wbu      = Module(new Wbu)
   val pcUpdate = Module(new PcUpdate)
 
-  val memRPort = Module(new SramRPort(XLen.W, 32.W))
   val readArb = Module(
     new GenericArbiter(new SramRPortReq(XLen.W), new SramRPortResp(32.W), 2)
   )
-  readArb.io.slaveReq      <> memRPort.io.req
-  readArb.io.slaveResp     <> memRPort.io.resp
   readArb.io.masterReq(0)  <> ifu.io.rReq
   readArb.io.masterResp(0) <> ifu.io.rResp
   readArb.io.masterReq(1)  <> lsu.io.rReq
   readArb.io.masterResp(1) <> lsu.io.rResp
+
+  val memRXbar = Module(
+    new Xbar(
+      new SramRPortReq(XLen.W),
+      new SramRPortResp(32.W),
+      Seq(
+        // "b00010000_00000000_0000????_????????".BP,
+        "b10000000_????????_????????_????0???".BP,
+        "b10000000_????????_????????_????1???".BP
+      ),
+      (req: SramRPortReq) => req.addr,
+      (resp: SramRPortResp) => resp.resp
+    )
+  )
+  memRXbar.io.masterReq  <> readArb.io.slaveReq
+  memRXbar.io.masterResp <> readArb.io.slaveResp
+
+  val memRPort1 = Module(new SramRPort(XLen.W, 32.W))
+  memRPort1.io.req  <> memRXbar.io.slaveReq(0)
+  memRPort1.io.resp <> memRXbar.io.slaveResp(0)
+  val memRPort2 = Module(new SramRPort(XLen.W, 32.W))
+  memRPort2.io.req  <> memRXbar.io.slaveReq(1)
+  memRPort2.io.resp <> memRXbar.io.slaveResp(1)
 
   val memWPort = Module(new SramWPort(XLen.W, 32.W))
   memWPort.io.req  <> lsu.io.wReq
--- npc/src/main/npc/Xbar.scala
+++ npc/src/main/npc/Xbar.scala
@@ -32,8 +32,22 @@ class GenericArbiter[TReq <: Data, TResp <: Data](
     trans := Mux(io.masterReq(i).valid, 1.B, Mux(io.masterResp(i).ready, 0.B, trans))
   }
 
+  object State extends CvtChiselEnum {
+    val S_Idle  = Value
+    val S_Trans = Value
+  }
+  import State._
+  private val y = RegInit(S_Idle)
+  y := MuxLookup(y, S_Idle)(
+    Seq(
+      S_Idle  -> Mux(arbiter.io.out.valid, S_Trans, S_Idle),
+      S_Trans -> Mux(io.slaveResp.ready, S_Idle, S_Trans)
+    )
+  )
+  private val inTrans = y === S_Trans
+
   // Connect arbiter states
-  for ((arbIn, req) <- arbiter.io.in.zip(transactions)) {
+  for (((arbIn, req), i) <- arbiter.io.in.zip(transactions).zipWithIndex) {
     arbIn.valid := req
   }
   arbiter.io.out.ready := MuxLookup(arbiter.io.chosen, 0.B)(
@@ -41,7 +55,9 @@ class GenericArbiter[TReq <: Data, TResp <: Data](
   )
 
   // Wire requests and responses
-  io.slaveReq.valid := arbiter.io.out.valid
+  io.slaveReq.valid := MuxLookup(arbiter.io.chosen, 0.B)(
+    (0 until n).map(i => i.asUInt -> (inTrans & io.masterReq(i).valid))
+  )
   io.slaveReq.bits := MuxLookup(arbiter.io.chosen, io.masterReq(0).bits)(
     (0 until n).map(i => i.asUInt -> io.masterReq(i).bits)
   )
@@ -49,12 +65,93 @@ class GenericArbiter[TReq <: Data, TResp <: Data](
     req.ready := (arbiter.io.chosen === i.asUInt) & io.slaveReq.ready
   }
   for ((resp, i) <- io.masterResp.zipWithIndex) {
-    resp.bits  := io.slaveResp.bits
     resp.valid := (arbiter.io.chosen === i.asUInt) & io.slaveResp.valid
+    resp.bits  := io.slaveResp.bits
   }
   io.slaveResp.ready := MuxLookup(arbiter.io.chosen, 0.B)(
     (0 until n).map(i => i.asUInt -> io.masterResp(i).ready)
   )
 
   io.chosen := arbiter.io.chosen
+}
+
+class XbarIO[TReq <: Data, TResp <: Data](
+  private val req:  TReq,
+  private val resp: TResp,
+  n:                Int)
+    extends Bundle {
+  val masterReq  = Flipped(Irrevocable(req))
+  val masterResp = Irrevocable(resp)
+  val slaveReq   = Vec(n, Irrevocable(req))
+  val slaveResp  = Flipped(Vec(n, Irrevocable(resp)))
+}
+
+class Xbar[TReq <: Data, TResp <: Data](
+  private val req:      TReq,
+  private val resp:     TResp,
+  private val addrPats: Seq[BitPat],
+  private val selAddr:  TReq => UInt,
+  private val selResp:  TResp => UInt)
+    extends Module {
+  val n  = addrPats.length
+  val io = IO(new XbarIO(req, resp, n))
+
+  private val addr = RegInit(0.U)
+
+  addr := Mux(io.masterReq.valid, selAddr(io.masterReq.bits), Mux(io.masterResp.ready, 0.U, addr))
+
+  private val addrSelDec = Decoder1H(addrPats.zipWithIndex)
+  private val addrSel1H  = addrSelDec(addr)
+  private val addrBad    = addrSel1H(addrSelDec.bitBad)
+
+  object State extends CvtChiselEnum {
+    val S_Idle  = Value
+    val S_Trans = Value
+  }
+  import State._
+  private val y = RegInit(S_Idle)
+  y := MuxLookup(y, S_Idle)(
+    Seq(
+      S_Idle  -> Mux(io.masterReq.valid, S_Trans, S_Idle),
+      S_Trans -> Mux(io.masterResp.ready, S_Idle, S_Trans)
+    )
+  )
+  private val inTrans = y === S_Trans
+
+  for ((slave, i) <- io.slaveReq.zipWithIndex) {
+    slave.valid := Mux(addrSel1H(i), io.masterReq.valid, 0.B)
+    slave.bits  := io.masterReq.bits
+  }
+  io.masterReq.ready := Mux(
+    inTrans,
+    Mux1H(
+      (0 until n).map(i => addrSel1H(i) -> io.slaveReq(i).ready) ++ Seq(
+        addrSel1H(addrSelDec.bitBad) -> io.masterReq.valid
+      )
+    ),
+    0.B
+  )
+
+  io.masterResp.valid := Mux(
+    inTrans,
+    Mux1H(
+      (0 until n).map(i => addrSel1H(i) -> io.slaveResp(i).valid) ++ Seq(
+        addrSel1H(addrSelDec.bitBad) -> 1.B
+      )
+    ),
+    0.B
+  )
+  io.masterResp.bits := Mux1H(
+    (0 until n).map(i => addrSel1H(i) -> io.slaveResp(i).bits) ++ Seq(
+      addrSel1H(addrSelDec.bitBad) -> io.slaveResp(0).bits
+    )
+  )
+  selResp(io.masterResp.bits) := Mux1H(
+    (0 until n).map(i => addrSel1H(i) -> selResp(io.slaveResp(i).bits)) ++ Seq(
+      addrSel1H(addrSelDec.bitBad) -> 3.U(2.W)
+    )
+  )
+  for ((slave, i) <- io.slaveResp.zipWithIndex) {
+    slave.ready := Mux(addrSel1H(i), io.masterResp.ready, 0.B)
+  }
 }
