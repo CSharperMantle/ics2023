ac4468fb29c9c03ef03fb2eebbdd97cbcceb90aa
Rong "Mantle" Bao <baorong2005@126.com>
2024-08-02 12:31:46 +0800

    ysyx: make rt-thread work on npc

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -11,7 +11,7 @@ extern char _heap_start;
 int main(const char *args);
 
 extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_SIZE (16 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
