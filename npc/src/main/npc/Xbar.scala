14337db8f2b17cf53718d5419649e1cb550267f0
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-07-29 11:06:42 +0800

    ysyx(npc): backport fixes for GenericArbiter

--- npc/src/main/npc/Xbar.scala
+++ npc/src/main/npc/Xbar.scala
@@ -1,6 +1,7 @@
 package npc
 
 import chisel3._
+import chisel3.experimental.requireIsChiselType
 import chisel3.util._
 
 import common._
@@ -31,74 +32,73 @@ class RRArbiter(n: Int) extends Module {
   class Port extends Bundle {
     val request = Input(Vec(n, Bool()))
     val done    = Input(Vec(n, Bool()))
-    val choice  = Output(UInt(log2Up(n).W))
+    val choice  = Output(Valid(UInt(log2Up(n).W)))
   }
   val io = IO(new Port)
 
   private val current = RegInit(0.U(log2Up(n).W))
+  private val next    = Mux(current === (n.U - 1.U), 0.U, current + 1.U)
 
   current := MuxCase(
     current,
     Seq(
-      (~io.request(current))                   -> Mux(current === (n.U - 1.U), 0.U, current + 1.U),
-      (io.request(current) & io.done(current)) -> Mux(current === (n.U - 1.U), 0.U, current + 1.U)
+      (!io.request(current))                     -> next,
+      (io.request(current) && !io.done(current)) -> current,
+      (io.request(current) && io.done(current))  -> next
     )
   )
 
-  io.choice := current
+  io.choice.valid := io.request(current)
+  io.choice.bits  := current
 }
 
 class GenericArbiter[TReq <: Data, TResp <: Data](
-  private val req:  TReq,
-  private val resp: TResp,
-  n:                Int)
+  reqGen:  TReq,
+  respGen: TResp,
+  n:       Int)
     extends Module {
+  requireIsChiselType(reqGen)
+  requireIsChiselType(respGen)
+
   class Port extends Bundle {
-    val masterReq  = Flipped(Vec(n, Irrevocable(req)))
-    val slaveReq   = Irrevocable(req)
-    val masterResp = Vec(n, Irrevocable(resp))
-    val slaveResp  = Flipped(Irrevocable(resp))
+    val masterReq  = Flipped(Vec(n, Decoupled(reqGen)))
+    val slaveReq   = Decoupled(reqGen)
+    val masterResp = Vec(n, Decoupled(respGen))
+    val slaveResp  = Flipped(Decoupled(respGen))
     val choice     = Output(UInt(log2Up(n).W))
   }
   val io = IO(new Port)
 
-  private val transactions = RegInit(VecInit(Seq.fill(n)(0.B)))
+  private val transactions = RegInit(VecInit(Seq.fill(n)(false.B)))
   for ((trans, i) <- transactions.zipWithIndex) {
-    trans := Mux(io.masterReq(i).valid, 1.B, Mux(io.masterResp(i).ready, 0.B, trans))
+    trans := Mux(io.masterReq(i).valid, true.B, Mux(io.masterResp(i).fire, false.B, trans))
   }
 
   private val arb = Module(new RRArbiter(n))
   for (i <- (0 until n)) {
     arb.io.request(i) := transactions(i)
-    arb.io.done(i)    := io.masterResp(i).ready
+    arb.io.done(i)    := io.masterResp(i).fire
   }
 
-  private val inTrans = RegInit(false.B)
-  inTrans := Mux(
-    io.masterReq(arb.io.choice).valid,
-    true.B,
-    Mux(io.slaveResp.ready, false.B, inTrans)
-  )
-
   // Wire requests and responses
-  io.slaveReq.valid := MuxLookup(arb.io.choice, 0.B)(
-    (0 until n).map(i => i.asUInt -> (inTrans & io.masterReq(i).valid))
+  io.slaveReq.valid := arb.io.choice.valid && MuxLookup(arb.io.choice.bits, false.B)(
+    (0 until n).map(i => i.asUInt -> io.masterReq(i).valid)
   )
-  io.slaveReq.bits := MuxLookup(arb.io.choice, io.masterReq(0).bits)(
+  io.slaveReq.bits := MuxLookup(arb.io.choice.bits, io.masterReq(0).bits)(
     (0 until n).map(i => i.asUInt -> io.masterReq(i).bits)
   )
   for ((req, i) <- io.masterReq.zipWithIndex) {
-    req.ready := (arb.io.choice === i.asUInt) & io.slaveReq.ready
+    req.ready := arb.io.choice.valid && (arb.io.choice.bits === i.asUInt) && io.slaveReq.ready
   }
   for ((resp, i) <- io.masterResp.zipWithIndex) {
-    resp.valid := (arb.io.choice === i.asUInt) & io.slaveResp.valid
+    resp.valid := arb.io.choice.valid && (arb.io.choice.bits === i.asUInt) && io.slaveResp.valid
     resp.bits  := io.slaveResp.bits
   }
-  io.slaveResp.ready := MuxLookup(arb.io.choice, 0.B)(
+  io.slaveResp.ready := arb.io.choice.valid && MuxLookup(arb.io.choice.bits, false.B)(
     (0 until n).map(i => i.asUInt -> io.masterResp(i).ready)
   )
 
-  io.choice := arb.io.choice
+  io.choice := arb.io.choice.bits
 }
 
 class Xbar[TReqBundle <: Data, TRespBundle <: Data, TResp <: Data](
